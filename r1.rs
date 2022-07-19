8c54265f4439aa1a1c744c7a417a3b30dff1d412
mwe85 <107491004+mwe85@users.noreply.github.com>
2022-07-19 19:42:04 -0400

    Update r1.rs

--- r1.rs
+++ r1.rs
@@ -1,2 +1,13 @@
 pub struct BoardState{
 }
+
+pub struct RCSConstraint{
+}
+
+pub struct HintMask{}
+
+impl HintMask{
+  pub fn fromRCSConstraint(constraint: RCSConstraint) -> HintMask{
+    HintMask{}
+  }
+}
